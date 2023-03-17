114f8178cecc05ff76493005f76f3cb77d4c3d7c
meghanathChappidi <121346936+meghanathChappidi@users.noreply.github.com>
2023-03-17 20:34:46 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,1 @@
+console.log("ia am indra")
---
+++ jsday1.js
@@ -0,0 +1,10 @@
+//let username = window.prompt("what is your name")
+//console.log(username);
+let username;
+
+document.getElementById("mybutton").onclick = function(){
+
+    username = document.getElementById("mytext").Value;
+    console.log(username);
+    
+}
